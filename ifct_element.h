4e65a95c939d6a20ef4f8b2696539520fa4ca76f
usoobin <112929005+usoobin@users.noreply.github.com>
2022-11-26 03:03:26 +0900

    20221126

--- ifct_element.h
+++ ifct_element.h
@@ -12,10 +12,10 @@
 #define N_HISTORY                   5
 #define MAX_PLACENAME               100
 
-void* ifctele_genElement(int index, int age, unsigned int detected_time, int history_place[N_HISTORY]);
-char* ifctele_getPlaceName(int placeIndex);
-int ifctele_getHistPlaceIndex(void* obj, int index);
-unsigned int ifctele_getinfestedTime(void* obj);
-int ifctele_getAge(void* obj);
-void ifctele_printElement(void* obj);
+void* ifctele_genElement(int index, int age, unsigned int detected_time, int history_place[N_HISTORY]); // 각 patient의 전체적인 정보 
+char* ifctele_getPlaceName(int placeIndex);																// 숫자를 입력 받아 장소 이릅 출력 
+int ifctele_getHistPlaceIndex(void* obj, int index);													// 감염 장소 순서에 대한 내용 
+unsigned int ifctele_getinfestedTime(void* obj);														// 감염 확인 일자 
+int ifctele_getAge(void* obj);																			// 감염된 환자의 나이 
+void ifctele_printElement(void* obj);																	// 값을 출력 
 #endif /* ifct_element_h */
